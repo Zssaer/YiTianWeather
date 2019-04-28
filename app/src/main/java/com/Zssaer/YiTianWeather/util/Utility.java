6c8ed97a14cac452c087a24ae86b4477ae7a92b3
zssaer <641348448@qq.com>
2019-04-29 01:21:09 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/Zssaer/YiTianWeather/util/Utility.java
+++ app/src/main/java/com/Zssaer/YiTianWeather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.Zssaer.YiTianWeather.db.City;
 import com.Zssaer.YiTianWeather.db.County;
 import com.Zssaer.YiTianWeather.db.Province;
+import com.Zssaer.YiTianWeather.gson.Weather;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
@@ -78,6 +80,22 @@ public class Utility {
             }
         }
         return false;
+    }
+
+
+    /*
+       将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonobject=new JSONObject(response);
+            JSONArray jsonArray=jsonobject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
